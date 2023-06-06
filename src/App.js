3af8bc12c3854c7468958cd4afec636ce4f57815
Vaibhav Bhalla <vaibhav.bhalla@basware.com>
2023-06-07 01:32:53 +0530

    programmatically navigate to other components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import { OrderSummary } from "./components/OrderSummary";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="about" element={<About />}></Route>
+        <Route path="order-summary" element={<OrderSummary />}></Route>
       </Routes>
     </>
   );
---
+++ src/components/OrderSummary.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom"
+
+export const OrderSummary = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+    <div>Order Confirmed</div>
+    <button onClick={() => navigate(-1)}>Go back</button>
+    </>
+  )
+}
