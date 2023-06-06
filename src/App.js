e46ebc7aac0ffbc516feafff33ba98d3c13a4f01
Vaibhav Bhalla <vaibhav.bhalla@basware.com>
2023-06-07 01:09:58 +0530

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="about" element={<About/>}></Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Routes>
+    </>
   );
 }
 
