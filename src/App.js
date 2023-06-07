f11620f2166549e7ca12b58f5459340872635d6c
Vaibhav Bhalla <vaibhav.bhalla@basware.com>
2023-06-07 13:46:50 +0530

    Lazy loading About component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
-import About from "./components/About";
 import Navbar from "./components/Navbar";
 import { OrderSummary } from "./components/OrderSummary";
 import { NoMatchRoute } from "./components/NoMatchRoute";
@@ -11,6 +11,8 @@ import { NewProducts } from "./components/NewProducts";
 import { Users } from "./components/Users";
 import { UserDetails } from "./components/UserDetails";
 import { Admin } from "./components/Admin";
+// import About from "./components/About";
+const LazyAbout = React.lazy(() => import("./components/About"));
 
 function App() {
   return (
@@ -18,16 +20,23 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="about" element={<About />}></Route>
+        <Route
+          path="about"
+          element={
+            <React.Suspense fallback="Loading...">
+              <LazyAbout />
+            </React.Suspense>
+          }
+        ></Route>
         <Route path="order-summary" element={<OrderSummary />}></Route>
         <Route path="products" element={<Products />}>
-          <Route index element={<FeaturedProducts/>}></Route>
-          <Route path="featured" element={<FeaturedProducts/>}></Route>
-          <Route path="new" element={<NewProducts/>}></Route>
+          <Route index element={<FeaturedProducts />}></Route>
+          <Route path="featured" element={<FeaturedProducts />}></Route>
+          <Route path="new" element={<NewProducts />}></Route>
         </Route>
-        <Route path="users" element={<Users/>}>
-          <Route path=":userId" element={<UserDetails/>}></Route>
-          <Route path="admin" element={<Admin/>}></Route>
+        <Route path="users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />}></Route>
+          <Route path="admin" element={<Admin />}></Route>
         </Route>
         <Route path="*" element={<NoMatchRoute />}></Route>
       </Routes>
