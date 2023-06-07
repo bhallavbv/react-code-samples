fe1a35fd52c0b4793f8fa061388a38370f48dc83
Vaibhav Bhalla <vaibhav.bhalla@basware.com>
2023-06-07 12:43:02 +0530

    Extract dynamic param value in component

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useParams } from "react-router-dom"
 
 export const UserDetails = () => {
+  const params = useParams();
+  const userId = params.userId;
   return (
-    <div>User details</div>
+    <div>Details about user with id - {userId}</div>
   )
 }
