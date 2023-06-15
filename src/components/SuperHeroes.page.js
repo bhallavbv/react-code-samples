d6e2209cc64fae59a360e795847106e764f30923
Vaibhav Bhalla <bhallavbv@gmail.com>
2023-06-15 18:53:05 +0530

    Fetch Superheros data using axios

--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export const SuperHeroesPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/superheroes").then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-    <div> Traditional SuperHeroes Page</div>
-  )
-}
+    <>
+      <h2>Super Heroes Page</h2>
+      {data.map((hero) => {
+        return <div>{hero.name}</div>;
+      })}
+    </>
+  );
+};
