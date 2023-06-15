b87ded63ac14934901c5f01a7f7db5a9fadfcfad
Vaibhav Bhalla <bhallavbv@gmail.com>
2023-06-15 19:05:12 +0530

    Use react query to fetch super heroes

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
 
 export const RQSuperHeroesPage = () => {
+  const { isLoading, data } = useQuery("super-heroes", () => {
+    return axios.get("http://localhost:4000/superheroes");
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
-    <div>RQSuperHeroes</div>
-  )
-}
+    <>
+      <h2>RQ Super Heroes</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}> {hero.name}</div>;
+      })}
+    </>
+  );
+};
