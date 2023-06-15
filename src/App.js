21f12011c43828e1a0f3816608d3b9328fd26e76
Vaibhav Bhalla <bhallavbv@gmail.com>
2023-06-15 18:56:41 +0530

    Setup for react query

--- src/App.js
+++ src/App.js
@@ -3,35 +3,39 @@ import "./App.css";
 import { HomePage } from "./components/Home.page";
 import { RQSuperHeroesPage } from "./components/RQSuperHeroes.page";
 import { SuperHeroesPage } from "./components/SuperHeroes.page";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/super-heroes">Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/super-heroes" element={<SuperHeroesPage />}></Route>
-
-          <Route
-            path="/rq-super-heroes"
-            element={<RQSuperHeroesPage />}
-          ></Route>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/super-heroes" element={<SuperHeroesPage />}></Route>
+            <Route
+              path="/rq-super-heroes"
+              element={<RQSuperHeroesPage />}
+            ></Route>
+            <Route path="/" element={<HomePage />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
