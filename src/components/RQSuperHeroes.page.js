5ca10212d88e0accac608e816dbe710d1bf1ae8f
Vaibhav Bhalla <bhallavbv@gmail.com>
2023-06-15 13:28:00 +0530

    Base setup - Setup routes and components

---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RQSuperHerosPage = () => {
+  return <div>RQ superheros page</div>;
+};
---
+++ src/components/SuperHeroes.page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SuperheroesPage = () => {
+  return <div>Traditional Super heroes page</div>;
+};
