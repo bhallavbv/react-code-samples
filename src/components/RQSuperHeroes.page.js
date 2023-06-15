0e202d19fc7d22aa2d5e2da6112e57daff918fca
Vaibhav Bhalla <bhallavbv@gmail.com>
2023-06-15 19:07:12 +0530

    refactor query declaration

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,10 +2,12 @@ import axios from "axios";
 import React from "react";
 import { useQuery } from "react-query";
 
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("super-heroes", () => {
-    return axios.get("http://localhost:4000/superheroes");
-  });
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
