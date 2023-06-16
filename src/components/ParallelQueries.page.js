9425a41c0711f7ee801887a271436497250e3581
Vaibhav Bhalla <bhallavbv@gmail.com>
2023-06-16 14:23:03 +0530

    Parallel queries

--- src/components/ParallelQueries.page.js
+++ src/components/ParallelQueries.page.js
@@ -1,6 +1,19 @@
+import axios from "axios";
 import React from "react";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
+const fetchFriends = () => {
+  return axios.get("https://jsonplaceholder.typicode.com/users");
+};
 
 export const ParallelQueriesPage = () => {
-  // Fetch superheroes and friends in this page
+  const { data: superheroes } = useQuery("super-heroes", fetchSuperHeroes);
+  const { data: friends } = useQuery("friends", fetchFriends);
+  
+  // can use (destructured and aliased) superheros and friends to render in jsx
   return <div>Parallel queryies page</div>;
 };
