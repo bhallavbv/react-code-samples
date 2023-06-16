78ed2817c423001104885d423df80d7f8350f698
Vaibhav Bhalla <bhallavbv@gmail.com>
2023-06-16 11:19:01 +0530

    Query by Id - query key holds all params from useQuery

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 
-const fetchSuperHero = (heroId) => {
+const fetchSuperHero = ({ queryKey }) => {
+  // queryKey = ["super-hero", heroId] as passed in useQuery line 11
+  const heroId = queryKey[1];
   return axios.get(`http://localhost:4000/superheroes/${heroId}`);
 };
 
 export const useSuperHeroData = (heroId) => {
-  return useQuery(["super-hero", heroId], () => fetchSuperHero(heroId), {
+  return useQuery(["super-hero", heroId], fetchSuperHero, {
     // onSuccess,
     // onError,
   });
